b6c39c110930356ca83e64bc1959ff2908a7c040
Wesley Kühn <wkuhn_dev@outlook.com>
2022-02-12 17:35:00 -0300

    Created base window based on a class.

---
+++ FreelancingHelper/Pages/BaseWindow.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace FreelancingHelper.Pages
+{
+    public class BaseWindow : Window
+    {
+        private string[] _appsPreferredFontFamilies = new string[]
+        {
+            "Hack",
+            "Calibri"
+        };
+
+        public BaseWindow()
+        {
+            ResizeMode = ResizeMode.CanMinimize;
+            WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            MouseDown += BaseWindow_MouseDown;
+            WindowStyle = WindowStyle.None;
+
+            var fontFamily = TryGetFirstFontFamilyOfSequence(_appsPreferredFontFamilies);
+            if (fontFamily != null)
+                FontFamily = fontFamily;
+        }
+
+        private FontFamily TryGetFirstFontFamilyOfSequence(string[] fontFamilies)
+        {
+            foreach (var fontFamily in fontFamilies)
+            {
+                var result = TryGetFontFamily(fontFamily);
+
+                if (result != null)
+                    return result;
+            }
+
+            return null;
+        }
+
+        private FontFamily TryGetFontFamily(string fontFamily)
+        {
+            FontFamily result;
+
+            try
+            {
+                result = new(fontFamily);
+
+                return result;
+            }
+            catch (ArgumentNullException)
+            {
+                return null;
+            }
+        }
+
+        private void BaseWindow_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                DragMove();
+        }
+    }
+}
--- FreelancingHelper/Pages/ConfigsWindow.xaml.cs
+++ FreelancingHelper/Pages/ConfigsWindow.xaml.cs
@@ -1,23 +1,13 @@
-﻿using System;
-using System.Windows;
-using System.Windows.Input;
-
-namespace FreelancingHelper.Pages
+﻿namespace FreelancingHelper.Pages
 {
     /// <summary>
     /// Interaction logic for ConfigsWindow.xaml
     /// </summary>
-    public partial class ConfigsWindow : Window
+    public partial class ConfigsWindow : BaseWindow
     {
         public ConfigsWindow()
         {
             InitializeComponent();
-        }
-
-        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            if (e.ChangedButton == MouseButton.Left)
-                DragMove();
         }
     }
 }
